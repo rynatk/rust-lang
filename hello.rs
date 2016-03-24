f663fcf5f1b3c90167541882908c10f72f34965c
Ryan Atkinson <ryanatk@vt.edu>
2016-03-24 07:56:30 -0400

    rust hello world

---
+++ hello.rs
@@ -0,0 +1,18 @@
+// This is a basic Rust comment
+
+fn main() {
+  // The statements here will be executed when the compiled binary is called
+
+  // Print text to console
+  println!("Hello World");
+
+  // Apparently single and double quotes are not interchangeable
+
+  // commpile with $ rustc hello.rs
+  // run binary with $ ./hello
+
+  println!("rustacean, rustafarian");
+
+  let x = 5 + /* 90 + */ 5; // wut; oh
+  println!("Is 'x' 10 or 100? x = {}", x); // 10
+}
